fc6d6da35c734be510f2ba73e025a7359a3f27e3
gehorak <horak.jiri@sendmail.cz>
2020-05-27 23:33:50 +0200

    Fix #05 - Typo repair

--- umbra/bin/start.sh
+++ umbra/bin/start.sh
@@ -2,4 +2,4 @@
 ulimit -c 0
 # https://github.com/internetarchive/umbra/blob/master/bin/drain-queue
 drain-queue --url amqp://guest:guest@rabbitmq:5672/%2f
-umbra -n 5 --executable /opt/bin/run-chromium  --url amqp://guest:guest@rabbitmq:5672/%2f
+umbra -n 5 --executable /opt/bin/run-chromium.sh --url amqp://guest:guest@rabbitmq:5672/%2f
